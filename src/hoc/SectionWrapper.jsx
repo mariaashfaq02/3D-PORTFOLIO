a8a4e6d8cd4884aaa73e6e02b4fe882a5be6df37
Maria Ashfaq <mariaashfaq.02@gmail.com>
2023-08-26 19:23:19 +0500

    Experience section made

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -6,8 +6,22 @@ import { staggerContainer } from '../utils/motion';
 
 const SectionWrapper = (Component,idName) => 
     function HOC(){
+        {/*staggerContainer will animate our section*/}
         return(
-            <motion.section>
+            <motion.section
+                variants={staggerContainer()}
+                initial="hidden"
+                whileInView="show"
+                viewport={{once:true,amount:0.25}}
+                className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+            >
+
+                {/*for the scrolling down
+                basically scrolling down to this invisible span section*/}
+                <span className='hash-span' id={idName}>
+                    &nbsp;
+                </span>
+
                 <Component/>
             </motion.section>
         )
